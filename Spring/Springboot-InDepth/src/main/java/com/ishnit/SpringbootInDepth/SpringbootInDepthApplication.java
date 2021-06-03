bdb6d864465da7e495be69ad6b403a26ba504314
Nirdesh Mohan <nirdeshm@hotmail.com>
2021-06-02 23:35:02 -0400

    initial

---
+++ Spring/Springboot-InDepth/src/main/java/com/ishnit/SpringbootInDepth/SpringbootInDepthApplication.java
@@ -0,0 +1,13 @@
+package com.ishnit.SpringbootInDepth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInDepthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInDepthApplication.class, args);
+	}
+
+}
---
+++ Spring/Springboot-InDepth/src/test/java/com/ishnit/SpringbootInDepth/SpringbootInDepthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishnit.SpringbootInDepth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInDepthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
