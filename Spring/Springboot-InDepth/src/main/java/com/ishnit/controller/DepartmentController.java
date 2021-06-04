03d67d4d711849ac63b22df7cc90185e91b167e6
Nirdesh Mohan <nirdeshm@hotmail.com>
2021-06-04 00:06:22 -0400

    lombok and logger

--- Spring/Springboot-InDepth/src/main/java/com/ishnit/controller/DepartmentController.java
+++ Spring/Springboot-InDepth/src/main/java/com/ishnit/controller/DepartmentController.java
@@ -2,6 +2,9 @@ package com.ishnit.controller;
 
 import com.ishnit.entity.Department;
 import com.ishnit.service.DepartmentService;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,34 +15,42 @@ public class DepartmentController {
     @Autowired
     DepartmentService service;
 
+    private final Logger log = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
     public Department createDepartment(@RequestBody Department department){
+        log.info("Adding New Department");
         return service.createDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> getDepartmentList(){
+        log.info("Get all Department");
         return service.getDepartmentList();
     }
 
     @GetMapping("/departments/{id}")
     public Department getDepartmentById(@PathVariable("id") Long deptId){
+        log.info("Get a Department");
         return service.getDepartmentById(deptId);
     }
 
     @DeleteMapping("/departments/{id}")
     public String deleteDepartmentById(@PathVariable("id") Long deptId){
+        log.info("Delete a Department");
         service.deleteDepartmentById(deptId);
         return "Department Deleted Successfully!!";
     }
 
     @PutMapping("/departments/{id}")
     public Department updateDepartmentById(@PathVariable("id") Long deptId, @RequestBody Department dept){
+        log.info("Update Department");
         return service.updateDepartmentById(deptId, dept);
     }
 
     @GetMapping("/departments/name/{name}")
     public Department getDepartmentByName(@PathVariable("name") String deptName){
+        log.info("Get Department by name");
         return service.getDepartmentByName(deptName);
     }
 }
--- Spring/Springboot-InDepth/src/main/java/com/ishnit/entity/Department.java
+++ Spring/Springboot-InDepth/src/main/java/com/ishnit/entity/Department.java
@@ -1,11 +1,20 @@
 package com.ishnit.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -13,55 +22,4 @@ public class Department {
     private String deptName;
     private String deptAddress;
     private String deptCode;
-
-    public long getDeptId() {
-        return deptId;
-    }
-
-    public void setDeptId(long deptId) {
-        this.deptId = deptId;
-    }
-
-    public String getDeptName() {
-        return deptName;
-    }
-
-    public void setDeptName(String deptName) {
-        this.deptName = deptName;
-    }
-
-    public String getDeptAddress() {
-        return deptAddress;
-    }
-
-    public void setDeptAddress(String deptAddress) {
-        this.deptAddress = deptAddress;
-    }
-
-    public String getDeptCode() {
-        return deptCode;
-    }
-
-    public void setDeptCode(String deptCode) {
-        this.deptCode = deptCode;
-    }
-
-    public Department() {super();}
-
-    public Department(long deptId, String deptName, String deptAddress, String deptCode) {
-        this.deptId = deptId;
-        this.deptName = deptName;
-        this.deptAddress = deptAddress;
-        this.deptCode = deptCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "deptId=" + deptId +
-                ", deptName='" + deptName + '\'' +
-                ", deptAddress='" + deptAddress + '\'' +
-                ", deptCode='" + deptCode + '\'' +
-                '}';
-    }
 }
