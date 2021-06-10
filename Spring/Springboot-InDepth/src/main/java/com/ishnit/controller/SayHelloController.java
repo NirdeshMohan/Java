3dcc25796cd2de04c9c3ed9ea52ca9d4ca5a963d
Nirdesh Mohan <nirdeshm@hotmail.com>
2021-06-10 17:44:01 -0400

    message from config

--- Spring/Springboot-InDepth/src/main/java/com/ishnit/controller/SayHelloController.java
+++ Spring/Springboot-InDepth/src/main/java/com/ishnit/controller/SayHelloController.java
@@ -1,12 +1,17 @@
 package com.ishnit.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SayHelloController {
+    @Value("${welcome.message}")
+    private String message;
+
     @GetMapping("/")
     public String sayHello(){
-        return "Hello From SpringBoot!!!!";
+        //return "Hello From SpringBoot!!!!";
+        return message;
     }
 }
