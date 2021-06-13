9b73bb572bb79806582701350d855584ed08ee0c
Nirdesh Mohan <nirdeshm@hotmail.com>
2021-06-13 09:55:48 -0400

    added acutator and hystris monitoring

---
+++ microservices/cloud-gateway/src/main/java/com/ishnit/cloud/gateway/FallBackMethodController.java
@@ -0,0 +1,18 @@
+package com.ishnit.cloud.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("userServiceFallBack")
+    public String userServiceFallBackMethod(){
+        return " User Service is taking too long to return response back, Please try again later.";
+    }
+
+    @GetMapping("departmentServiceFallBack")
+    public String departmentServiceFallBackMethod(){
+        return " Department Service is taking too long to return response back, Please try again later.";
+    }
+}
