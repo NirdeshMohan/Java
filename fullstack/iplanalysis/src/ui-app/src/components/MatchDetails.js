83924152fed784eece76cfe70c9fc0ae9e1bdbfc
Nirdesh Mohan <nirdeshm@hotmail.com>
2021-06-19 19:15:05 -0400

    Remove UI from Java

--- fullstack/iplanalysis/src/ui-app/src/components/MatchDetails.js
+++
@@ -1,8 +0,0 @@
-export const MatchDetails = () => {
-    return (
-      <div className="MatchDetails">
-        <h2>Latest Matches</h2>
-        <h3>Match Details</h3>
-      </div>
-    );
-}
--- fullstack/iplanalysis/src/ui-app/src/components/MatchShortDetails.js
+++
@@ -1,7 +0,0 @@
-export const MatchShortDetails = () => {
-    return (
-      <div className="MatchShortDetails">
-        <h4>Match ShortDetails</h4>
-      </div>
-    );
-}
