21e028dfd4f4427062cbcbed85f59fdb39d96805
Nirdesh Mohan <nirdeshm@gmail.com>
2021-09-27 22:11:44 -0400

    First Commit

---
+++ Spring/booksread-data-loader/src/main/java/com/ishnit/booksreaddataloader/BooksreadDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.ishnit.booksreaddataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksreadDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksreadDataLoaderApplication.class, args);
+	}
+
+}
---
+++ Spring/booksread-data-loader/src/test/java/com/ishnit/booksreaddataloader/BooksreadDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishnit.booksreaddataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksreadDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
